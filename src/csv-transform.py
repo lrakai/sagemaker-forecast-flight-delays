d7c2f9870c344715eb5d569fb459991cc449dbb7
Logan Rakai <lmrakai@gmail.com>
2019-04-16 00:02:00 -0600

    Implement more of the transform

--- src/csv-transform.py
+++ src/csv-transform.py
@@ -1,9 +1,11 @@
+import math
+import os
 import pandas as pd
 import numpy as np
 import matplotlib as plt
 
 
-def csv_to_df(csv_path):
+def csv_to_df(csv_path, skiprows):
     dtypes = {
         "YEAR":  np.int64,
         "QUARTER":  "category",
@@ -19,7 +21,7 @@ def csv_to_df(csv_path):
         "DEP_TIME":  np.int64,
         "DEP_DELAY":  np.float64,
         "DEP_DELAY_NEW":  np.float64,
-        "DEP_DEL15":  "category",
+        "DEP_DEL15":   np.int64,
         "DEP_DELAY_GROUP":  np.int64,
         "CRS_ARR_TIME":  np.int64,
         "ARR_DELAY":  np.float64,
@@ -27,19 +29,43 @@ def csv_to_df(csv_path):
         "DISTANCE":  np.float64,
         "DISTANCE_GROUP":  "category"
     }
-    df = pd.read_csv(csv_path, dtype=dtypes)
+    df = pd.read_csv(csv_path, dtype=dtypes, skiprows=skiprows)
     return df
 
 
 def onehot_encode(df):
+    return df
+
+
+def write_csv(df, csv_path, transform_name):
+    out_path = "{0}_{1}.csv".format(
+        os.path.splitext(csv_path)[0], transform_name)
+    df.to_csv(path_or_buf=out_path, index=False)
     return
 
 
-def csv_transform(csv_path, transform_name):
-    df = csv_to_df(csv_path)
-    df = df.drop(columns=['YEAR'])
+def csv_transform(csv_path, drop_columns, data_config):
+    df = csv_to_df(csv_path, data_config["skiprows"])
+    df = df.drop(columns=drop_columns)
+    df = onehot_encode(df)
+    write_csv(df, csv_path, data_config["transform_name"])
     return
 
 
-if __name__ == '__main__':
-    csv_transform("data/Flights_Dec2016-Nov2017_sampled.csv", "simple")
+if __name__ == "__main__":
+    drop_columns = ["YEAR", "TAIL_NUM", "FL_NUM"]
+    numrows = 1048576 - 1  # minus 1 for header
+    training_ratio = 0.7
+    data_config = {
+        "train": {
+            "skiprows": range(math.floor(training_ratio * numrows) + 1, numrows + 2),
+            "skipfooter": numrows - math.floor(training_ratio * numrows),
+            "transform_name": "training"
+        },
+        "test": {
+            "skiprows": range(1, math.floor(training_ratio * numrows) + 1),
+            "transform_name": "test"
+        }
+    }
+    csv_transform("data/Flights.csv", drop_columns, data_config["train"])
+    csv_transform("data/Flights.csv", drop_columns, data_config["test"])
